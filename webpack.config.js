38977a6a14f19000c236d5f64b53dacb1fa96f2f
Preda Anuvatnujotikul <preda.anu@gmail.com>
2017-07-31 16:56:46 -0700

    Configured webpack and included script tags for style sheet in index.html

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  entry: ‘./react-client/src/index.js’,
+  output: {
+    path: './react-client/dist',
+    filename: ‘bundle.js’,
+  },
+  module : {
+    loaders : [
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.png$/,
+        loader: 'url-loader?limit=100000&minetype=image/png'
+      },
+      {
+        test: /\.jpg/,
+        loader: 'file-loader'
+      },
+      {
+        test : /\.jsx?/,
+        include : SRC_DIR,
+        loader : 'babel-loader',      
+        query: {
+          presets: ['react', 'es2015']
+       }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ]
+};
