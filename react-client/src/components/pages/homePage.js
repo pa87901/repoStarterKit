f80244da0c6df1512549562d2c052c9550631146
cameronryde <cryde@invaluable.com>
2020-02-20 14:07:05 -0500

    Added some stuff to the homepage

--- react-client/src/components/pages/homePage.js
+++ react-client/src/components/pages/homePage.js
@@ -4,7 +4,29 @@ class HomePage extends Component {
   render() {
     return (
       <div>
-        Welcome to the website
+        <section id="colorlib-hero" className="js-fullheight" data-section="home">
+          <div className="flexslider js-fullheight">
+            <ul className="slides">
+            <li style={{backgroundImage: 'url(images/img_bg_1.jpg)'}}>
+              <div className="overlay">
+              <div className="container-fluid">
+              <div className="row">
+                  <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                  <div className="slider-text-inner js-fullheight">
+                      <div className="desc">
+                      <h1>Hi! <br />I'm Cameron</h1>
+                      <h2>Welcome to my website</h2>
+                      <p><a className="btn btn-primary btn-learn">Download CV <em className="icon-download4" /></a></p>
+                      </div>
+                  </div>  
+                  </div>
+              </div>  
+              </div>
+              </div>
+            </li>
+            </ul>
+          </div>
+        </section>
       </div>
     )
   }
